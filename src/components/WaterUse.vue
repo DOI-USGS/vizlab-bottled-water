204237a0b6de62c4e1ec9468fda43697cc2758df
Corson-Dosch <hcorson-dosch@usgs.gov>
2023-08-24 12:46:05 -0500

    Update mockup of water use section

--- src/components/WaterUse.vue
+++ src/components/WaterUse.vue
@@ -1,98 +1,126 @@
 <template>
   <section id="water_use">
-  <div id="grid-container">
-        <div id="title">
-            <h3> How much water are bottling facilities using?
-            </h3>
+    <div id="grid-container-use">
+      <div id="title">
+          <h3> 
+            How much water are bottling facilities using?
+          </h3>
+      </div>
+      <div id="text">
+        <div class="text-container">
+          <p>
+            Water use data can be difficult to find for beverage-bottling facilities. Most states don't make these data publicly available, and some states even have laws prohibiting the sharing of these data. Because the data are limited, we can't make broad generalizations about the water usage of bottling facilities. However, we do have fairly comprehensive data for some facilities across the U.S. The charts below show how much data is available, and how the amount of water used in bottled water facilities varies by water source.  
+          </p>
         </div>
-        <div class="text-container"> <div class="text-container"><p>
-          Water use data can be difficult to find for beverage-bottling facilities. Most states don’t make these data publicly available, and some states even have laws prohibiting the sharing of these data. Because the data are limited, we can’t make broad generalizations about the water usage of bottling facilities. However, we do have fairly comprehensive data from a few counties across the U.S. The charts below show how the amount of water used for bottling varies by the type of facility in each of these counties.  
-        </p></div></div>
-  </div>
-</section>
+      </div>
+      <div id="img-wu-sites-map" class="img-container">
+        <div class="text-container">
+          <p>
+            How many facilities have water use data?
+          </p>
+        </div>
+        <img src="../assets/images/sites_figure_wateruse.png">
+      </div>
+      <div id="img-wu-sites-bar" class="img-container">
+        <div class="text-container">
+          <p>For what types of facilities do we have water use data?
+          </p>
+        </div>
+        <img src="../assets/images/ac_state_wu_bar.png">
+      </div>
+      <div class="img-bw-use-map">
+        <div class="text-container">
+          <p>Among bottled water facilities with data, how much water is used?
+          </p>
+        </div>
+        <img src="../assets/images/ac_facility_wu_ps_map.png">
+      </div>
+      <div class="img-bw-use-source">
+        <div class="text-container">
+          <p>And how does that use vary by water source?
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
-import { isMobile } from 'mobile-device-detect';
+  import { isMobile } from 'mobile-device-detect';
 
-export default {
-  name: "WaterUse",
-  components: {
+  export default {
+    name: "WaterUse",
+    components: {
+    },
+    props: {
+      data: Object
+    },
+    data() {
+      return {
 
-  },
-  props: {
-    data: Object
-  },
-  data() {
-    return {
-
-      publicPath: import.meta.env.BASE_URL, // find the files when on different deployment roots
-      mobileView: isMobile, // test for mobile
-      
-    }
-  },
-  mounted(){      
-    const self = this;
-
- 
-  },
-  methods:{
-    isMobile() {
-            if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-                return true
-            } else {
-                return false
-            }
+        publicPath: import.meta.env.BASE_URL, // find the files when on different deployment roots
+        mobileView: isMobile, // test for mobile
+        
+      }
+    },
+    mounted(){      
+      const self = this;
+    },
+    methods:{
+      isMobile() {
+              if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                  return true
+              } else {
+                  return false
+              }
+      }
     }
   }
-}
 </script>
 <style scoped lang="scss">
+  $pal_red: '#FD5901';
+  $pal_or: '#F78104';
+  $pal_yell: '#FAAB36';
+  $pal_teal: '#008083';
+  $pal_blue_dark: '#042054';
 
-$pal_red: '#FD5901';
-$pal_or: '#F78104';
-$pal_yell: '#FAAB36';
-$pal_teal: '#008083';
-$pal_blue_dark: '#042054';
-
-
-#map-container {
+  #grid-container-use {
     display: grid;
-    grid-template-columns: 1fr 3fr;
-    grid-template-rows: 0.5fr 3fr;
+    grid-template-columns: 1fr;
+    grid-template-rows: 0.5fr max-content max-content max-content max-content max-content;
     grid-template-areas:
-      "title title"
-      "chart map";
+      "title"
+      "text"
+      "wu-sites-map"
+      "wu-sites-bar"
+      "bw-use-map"
+      "bw-use-source";
     justify-content: center;
     margin: auto;
     max-width: 1600px;
-    //height: 88vh;
-}
-
-#oconus-container {
-    grid-area: map;
-    align-self: center;
-}
-
-#chart-container {
-    grid-area: chart;
-    align-self: center;
-}
-
-#title {
+  }
+  #title {
     grid-area: title;
     align-self: center;
     font-size: 20px;
     font-family: sans-serif;
-    //background-color: red;
-}
-
-.dropdown {
-    font-size: 20px;
-    /* display: flex;
-    flex-direction: row;
-    transition: width 2s, height 2s, transform 2s;
-    will-change: width; */
-}
-
-
+  }
+  #text {
+    grid-area: text;
+    justify-self: start;
+  }
+  #img-wu-sites-map {
+    grid-area: wu-sites-map;
+  }
+  #img-wu-sites-bar {
+    grid-area: wu-sites-bar;
+  }
+  #img-bw-use-map {
+    grid-area: bw-use-map;
+  }
+  #img-bw-use-source {
+    grid-area: bw-use-source;
+  }
+  .img-container {
+    max-width: 100vw;
+  }
 </style>
