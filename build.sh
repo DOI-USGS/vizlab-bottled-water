9ac479eda77b26c414eaab0c0680a42a33fcfcab
Cee Nell <cnell@usgs.gov>
2023-08-30 21:42:49 -0700

    use dev script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo "this is the var $E_VUE_BUILD_MODE"
 if [ "$E_VUE_BUILD_MODE" = "test" ]
-then npm run build-test
+then npm run dev
 elif [ "$E_VUE_BUILD_MODE" = "beta" ]
 then npm run build-beta
 else npm run build
