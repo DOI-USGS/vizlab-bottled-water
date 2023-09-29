683d7461de429c9dd133515526645a03e34ac229
elmeraa <eazadpour@usgs.gov>
2023-09-29 09:59:59 -0700

    more exploratory bits for design studio

--- avg_annual_bw_use_expl.R
+++ avg_annual_bw_use_expl.R
@@ -39,6 +39,15 @@ font_add_google(font_legend)
 showtext_opts(dpi = 300, regular.wt = 200, bold.wt = 700)
 showtext_auto(enable = TRUE)
 
+# cowplot
+plot_margin <- 0.025
+
+canvas <- grid::rectGrob(
+  x = 0, y = 0,
+  width = width, height = height,
+  gp = grid::gpar(fill = bkgd_color, alpha = 1, col = bkgd_color)
+)
+
 # get average annaul water use for each year by water source
 bw_only_inventory_avg_yr_mgd <- bw_only_inventory_sf |>
   filter(wu_data_flag == "Y",
@@ -99,16 +108,6 @@ map_bw_avg_wu_facet_yr <- ggplot() +
         ) +
   facet_wrap(~year_of_data, nrow = 5)
 
-# cowplot
-plot_margin <- 0.025
-
-canvas <- grid::rectGrob(
-  x = 0, y = 0,
-  width = width, height = height,
-  gp = grid::gpar(fill = bkgd_color, alpha = 1, col = bkgd_color)
-)
-
-
 plt_yr <- ggdraw(ylim = c(0,1), # 0-1 scale makes it easy to place viz items on canvas
               xlim = c(0,1)) +
   # a background
@@ -139,7 +138,6 @@ plt_yr <- ggdraw(ylim = c(0,1), # 0-1 scale makes it easy to place viz items on 
 
 ggsave("3_visualize/out/explore_average_annual_bw_use_map.png", plt_yr, width = width, height = height, dpi = dpi, bg = bkgd_color)
 
-
 ##### proportional symbol map of average annual MGD of use - colored by # of years of data #####
 bw_avg_annual_facet_source <- ggplot() +
   geom_sf(data = conus_sf,
@@ -167,7 +165,6 @@ bw_avg_annual_facet_source <- ggplot() +
         legend.key.width = unit(2, "cm"),
         legend.key.height = unit(0.75, "cm")
   )
-
 
 plt_src <- ggdraw(ylim = c(0,1), # 0-1 scale makes it easy to place viz items on canvas
               xlim = c(0,1)) +
@@ -198,7 +195,6 @@ plt_src <- ggdraw(ylim = c(0,1), # 0-1 scale makes it easy to place viz items on
 
   ggsave("3_visualize/out/avg_annual_mgd_by_year.png", plt_src, width = width, height = height, dpi = dpi, bg = bkgd_color)
 
-
   #####  histogram of # of years of data across BW facilities #####
   bw_only_inventory_yr_count <- bw_only_inventory_sf |>
     filter(wu_data_flag == "Y",
@@ -212,7 +208,82 @@ plt_src <- ggdraw(ylim = c(0,1), # 0-1 scale makes it easy to place viz items on
     labs(y = "Count of bottled water facilities", x = NULL) +
     theme_minimal() +
     theme(text = element_text(family = font_legend, size = 14),
-          panel.grid.minor = element_blank()) +
+          panel.grid.minor = element_blank(),
+          axis.title.y = element_text(margin = margin(r = 10))) +
     scale_y_continuous(limits = c(0, 75), breaks = seq(0, 75, by = 15))
 
-  ggsave("3_visualize/out/hist_yrs_bw_data.png",hist_bw,  width = width, height = height, dpi = dpi, bg = bkgd_color)
+  #####  bar plot showing total # of years of data and the y axis count of bottled water facilities  #####
+  total_yrs_bw_facilities <- bw_only_inventory_yr_count |>
+    summarize(total_years = n_distinct(year_of_data),
+              total_count = sum(count))
+
+  total_yrs_bw_facilities_barplot <- ggplot(total_yrs_bw_facilities, aes(x = total_years, y = total_count)) +
+    geom_bar(stat = "identity", fill = "#1599CF", color = "grey80", width = 0.1) +
+    labs(y = "Total count of bottled water facilities", x = "Total number of years of data") +
+    theme_minimal() +
+    theme(text = element_text(family = font_legend, size = 14),
+          panel.grid.minor = element_blank(),
+          axis.title.y = element_text(margin = margin(r = 10)),
+          axis.title.x = element_text(margin = margin(t = 10))) +
+    scale_x_continuous(breaks = total_yrs_bw_facilities$total_years, labels = total_yrs_bw_facilities$total_years)
+
+  # combine plots
+  combined_plot <- plot_grid(hist_bw, total_yrs_bw_facilities_barplot, ncol = 2, rel_widths = c(0.9, 0.25))
+
+  ggsave("3_visualize/out/yrs_bw_facilities_barplot.png",
+         combined_plot,  width = width, height = height, dpi = dpi, bg = bkgd_color)
+
+  #####  map where color = # of years for which there are bottled water data  #####
+  bw_only_inventory_yr_count_sf <- bw_only_inventory_sf |>
+    filter(wu_data_flag == "Y",
+           !year_of_data == -9999) |>
+    group_by(year_of_data) |>
+    summarize(count = n())
+
+  count_yrs_bw_map <- ggplot() +
+    geom_sf(data = conus_sf,
+            fill = bkgd_color,
+            color = conus_outline_col,
+            size = 0.6,
+            linetype = "solid") +
+    geom_sf(data = bw_only_inventory_yr_count_sf,
+            aes(geometry = geometry,  color = count, fill = count),
+            pch = 21,
+            stroke = 0.6,
+            color = bkgd_color,
+            alpha = 0.75,
+            size = 5) +
+    scale_fill_viridis_b(name = "Count of years with bottled water data", alpha = 0.75,
+                         guide = guide_colorsteps(title.position = "top", title.hjust = 0.5,
+                                            override.aes = list(color = bkgd_color),
+                                            nrow = 1)) +
+    theme_void() +
+    theme(legend.position = "top",
+          text = element_text(family = font_legend, size = 14),
+          legend.key.width = unit(2, "cm"),
+          legend.key.height = unit(0.75, "cm")
+    )
+
+  plt_count_yrs_map <- ggdraw(ylim = c(0,1), # 0-1 scale makes it easy to place viz items on canvas
+                    xlim = c(0,1)) +
+    # a background
+    draw_grob(canvas,
+              x = 0, y = 1,
+              height = height, width = width,
+              hjust = 0, vjust = 1) +
+    draw_plot(count_yrs_bw_map + theme(legend.position = "none"),
+              x = 0.995,
+              y = -0.01,
+              height = 0.95,
+              width = 1 - plot_margin,
+              hjust = 1,
+              vjust = 0) +
+    # color legend
+    draw_plot(cowplot::get_legend(count_yrs_bw_map),
+              x = 0.1,
+              y = 0.3,
+              height = 1.2,
+              width = 0.8)
+
+  ggsave("3_visualize/out/count_yrs_bw_data_map.png",
+         plt_count_yrs_map, width = width, height = height, dpi = dpi, bg = bkgd_color)
