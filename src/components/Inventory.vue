a9cb929d15896fe7a3ab6d7e2709b703b5d57880
Corson-Dosch <hcorson-dosch@usgs.gov>
2023-10-05 17:40:22 -0500

    Clean up inventory text, float pictogram

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -2,51 +2,30 @@
     <section id="inventory_intro">
         <div id="grid-container-inventory">
             <div id="title">
-                <h3>
-                    Where is water being bottled?
-                </h3>
+               <h3 v-html="inventoryText.title" />
             </div>
             <br>
             <OCONUS id="oconus"/>
-            <div id="text">
-                <div class="text-container">
-                    <p>
-                        There are many type of facilities where water is the primary part of a beverage that is bottled.  The USGS has compiled a list of facilities for several beverage types with the main focus on bottled water (Buchwald and others, 2023). 
-                    </p>
-                </div>
-                <div class="text-container">
-                    <p>
-                        The bottled water industry estimates that the United States consumed 15 billion gallons (57 billion liters) of bottled water in 2020 (International Bottled Water Association). That's 45 gallons of bottled water per person. 
-                    </p>
-                </div>
-                <img src="../assets/images/bottled-water_consumption-pictogram_small_2023-09-19.png">
-                <div class="text-container">
-                    <p>
-                        Despite what we know about bottled water consumption, national datasets about how much water is withdrawn from groundwater, surface water, or springs for bottled water and other beverages are limited. Reporting is  variable, and some states do not permit public sharing of beverage-bottling data. Without these data, we cannot accurately assess bottled water influence on the water cycle locally, regionally, or nationally.  
-                    </p>
-                </div>
-                <div class="text-container">
-                    <p>
-                        To help fill this gap, the USGS Water Mission Area's Bottled Water Assessment project conducted an inventory of bottling facilities across the United States (LINK TO DATA RELEASE). The inventory helps answer the following questions:  
-                    </p>
-                </div>
-                <div class="text-container">
-                    <li>
-                        How many beverage bottling facilities exist in the U.S., and where are they located?
-                    </li>
-                    <li>
-                        Where are these facilities sourcing their water (e.g., groundwater, spring water, public supply)?
-                    </li>
-                    <li>
-                        How much water are bottling facilities using to make their products? 
-                    </li>
-                </div>
+            <div id="text" class="text-container">
+                <p v-html="inventoryText.paragraph1" />
+                <br>
+                <img class='image-float' src="../assets/images/bottled-water_consumption-pictogram_small_2023-09-19.png">
+                <p v-html="inventoryText.paragraph2" />
+                <br>
+                <p v-html="inventoryText.paragraph3" />
+                <br>
+                <p v-html="inventoryText.paragraph4" />
+                <br>
+                <li v-html="inventoryText.bullet1" />
+                <li v-html="inventoryText.bullet2" />
+                <li v-html="inventoryText.bullet3" />
             </div>
         </div>
     </section>
 </template>
 <script>
 import { isMobile } from 'mobile-device-detect';
+import inventoryText from "./../assets/text/inventoryText.js";
 
 export default {
   name: "Iventory",
@@ -65,6 +44,7 @@ export default {
       publicPath: import.meta.env.BASE_URL, // find the files when on different deployment roots
       mobileView: isMobile, // test for mobile
       
+      inventoryText: inventoryText.inventoryText
     }
   },
   mounted(){      
@@ -113,5 +93,11 @@ export default {
     #text {
         grid-area: text;
         justify-self: start;
+    }
+    .image-float {
+        float: right;
+        width: 50vw;
+        max-width: 1000px;
+        margin: 0px 0px 1rem 1rem;
     }
 </style>
