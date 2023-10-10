9cda7fbcb99429b935a6dded178868e7102f399d
Corson-Dosch <hcorson-dosch@usgs.gov>
2023-10-10 10:36:58 -0500

    set up grid for water source section

--- src/components/WaterSource.vue
+++ src/components/WaterSource.vue
@@ -1,52 +1,56 @@
 <template>
   <section id="water_source">
-    <div class="text-container">
-      <h3 v-html="sourceText.title" />
-      <p v-html="sourceText.paragraph1" />
-    </div>
-    <div id="grid-container-illustration">
-      <div id="illustration-public-supply" class="img-container">
-        <img class="illustration" src="../assets/images/public-supply.png">
+    <div id="grid-container-source">
+      <div id="title" class="text-container">
+        <h3 v-html="sourceText.title" />
       </div>
-      <div id="illustration-self-supply" class="img-container">
-        <img class="illustration" src="../assets/images/self-supply.png">
+      <div id="text1" class="text-container">
+        <p v-html="sourceText.paragraph1" />
       </div>
-    </div>
-    <div class="text-container">
-      <p v-html="sourceText.paragraph2" />
-      <br>
-      <p v-html="sourceText.subtitle1" />
-    </div>
-    <div id="img-ws-national-types" class="img-container">
-      <img src="../assets/images/perc_expanded_self_supply_barplot.png">
-      <img src="../assets/images/count_expanded_self_supply_barplot.png">
-    </div>
-    <div class="text-container">
-      <p v-html="sourceText.paragraph3" />
-    </div>
-    <div v-if="!mobileView" id="source-maps-container">
-      <div id="source-count-percent" class="img-container">
-        <img class="source-map" src="../assets/images/perc_count_bottled_water_map.png">
+      <div id="grid-container-illustration">
+        <div id="img-illustration-public-supply" class="img-container">
+          <img class="illustration" src="../assets/images/public-supply.png">
+        </div>
+        <div id="img-illustration-self-supply" class="img-container">
+          <img class="illustration" src="../assets/images/self-supply.png">
+        </div>
       </div>
-    </div>
-    <div v-if="mobileView" id="grid-container-source-maps">
-      <div id="source-self-count" class="img-container">
-        <img class="source-map" src="../assets/images/map_bottled_water_self_supply_count.png">
+      <div id="text2" class="text-container">
+        <p v-html="sourceText.paragraph2" />
+        <br>
+        <p v-html="sourceText.subtitle1" />
       </div>
-      <div id="source-self-percent" class="img-container">
-        <img class="source-map" src="../assets/images/map_bottled_water_self_supply_perc.png">
+      <div id="img-ws-national-types" class="img-container">
+        <img src="../assets/images/perc_expanded_self_supply_barplot.png">
+        <img src="../assets/images/count_expanded_self_supply_barplot.png">
       </div>
-      <div id="source-combo-count" class="img-container">
-        <img class="source-map" src="../assets/images/map_bottled_water_combination_count.png">
+      <div id="text3" class="text-container">
+        <p v-html="sourceText.paragraph3" />
       </div>
-      <div id="source-combo-percent" class="img-container">
-        <img class="source-map" src="../assets/images/map_bottled_water_combination_perc.png">
+      <div v-if="!mobileView" id="source-maps-container">
+        <div id="img-source-count-percent" class="img-container">
+          <img class="source-map" src="../assets/images/perc_count_bottled_water_map.png">
+        </div>
       </div>
-      <div id="source-public-count" class="img-container">
-        <img class="source-map" src="../assets/images/map_bottled_water_public_supply_count.png">
-      </div>
-      <div id="source-public-percent" class="img-container">
-        <img class="source-map" src="../assets/images/map_bottled_water_public_supply_perc.png">
+      <div v-if="mobileView" id="grid-container-source-maps">
+        <div id="img-source-self-count" class="img-container">
+          <img class="source-map" src="../assets/images/map_bottled_water_self_supply_count.png">
+        </div>
+        <div id="img-source-self-percent" class="img-container">
+          <img class="source-map" src="../assets/images/map_bottled_water_self_supply_perc.png">
+        </div>
+        <div id="img-source-combo-count" class="img-container">
+          <img class="source-map" src="../assets/images/map_bottled_water_combination_count.png">
+        </div>
+        <div id="img-source-combo-percent" class="img-container">
+          <img class="source-map" src="../assets/images/map_bottled_water_combination_perc.png">
+        </div>
+        <div id="img-source-public-count" class="img-container">
+          <img class="source-map" src="../assets/images/map_bottled_water_public_supply_count.png">
+        </div>
+        <div id="source-public-percent" class="img-container">
+          <img class="source-map" src="../assets/images/map_bottled_water_public_supply_perc.png">
+        </div>
       </div>
     </div>
   </section>
@@ -95,39 +99,74 @@
   $pal_teal: '#008083';
   $pal_blue_dark: '#042054';
 
+  #grid-container-source {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 0.5fr max-content max-content max-content max-content;
+    grid-template-areas:
+      "title"
+      "text1"
+      "illustration"
+      "text2"
+      "source-bars"
+      "text3"
+      "source-maps";
+    justify-content: center;
+    margin: auto;
+    max-width: 1600px;
+  }
+  #title {
+    grid-area: title;
+    justify-self: start;
+  }
+  #text1 {
+    grid-area: text1;
+    justify-self: start;
+  }
+  #text2 {
+    grid-area: text2;
+    justify-self: start;
+  }
+  #img-ws-national-types {
+    grid-area: source-bars;
+    justify-self: center;
+  }
+  #text3 {
+    grid-area: text3;
+    justify-self: start;
+  }
   #grid-container-illustration {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      grid-template-rows: max-content;
+    grid-area: illustration;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: max-content;
+    grid-template-areas:
+      "public-supply self-supply";
+    justify-content: center;
+    margin: auto;
+    max-width: 1600px;
+    @media screen and (max-width: 600px) {
+      grid-template-columns: 1fr;
+      grid-template-rows: max-content max-content;
       grid-template-areas:
-        "public-supply self-supply";
-      justify-content: center;
-      margin: auto;
-      max-width: 1600px;
-      @media screen and (max-width: 600px) {
-        grid-template-columns: 1fr;
-        grid-template-rows: max-content max-content;
-        grid-template-areas:
-          "public-supply"
-          "self-supply";
-      }
+        "public-supply"
+        "self-supply";
+    }
   }
   .img-container {
     max-width: 100vw;
   }
-  #illustration-public-supply {
+  #img-illustration-public-supply {
     grid-area: public-supply;
   }
-  #illustration-self-supply {
+  #img-illustration-self-supply {
     grid-area: self-supply;
-  }
-  img {
-    max-width: 100%;
   }
   .illustration {
     max-width: 100%;
   }
   #grid-container-source-maps {
+    grid-area: source-maps;
     display: grid;
     grid-template-columns: 1fr;
         grid-template-rows: max-content max-content max-content max-content max-content max-content;
@@ -142,22 +181,22 @@
       margin: auto;
       max-width: 1600px;
   }
-  #source-self-count {
+  #img-source-self-count {
     grid-area: count-self;
   }
-  #source-combo-count {
+  #img-source-combo-count {
     grid-area: count-combo;
   }
-  #source-public-count {
+  #img-source-public-count {
     grid-area: count-public;
   }
-  #source-self-percent {
+  #img-source-self-percent {
     grid-area: perc-self;
   }
-  #source-combo-percent {
+  #img-source-combo-percent {
     grid-area: perc-combo;
   }
-  #source-public-percent {
+  #img-source-public-percent {
     grid-area: perc-public;
   }
   .source-map {
