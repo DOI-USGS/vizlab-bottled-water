262133985055982793e89a883e70026b3f8e52db
Carr <ancarr@usgs.gov>
2023-10-13 16:25:45 -0600

    edited text

--- src/assets/text/inventoryText.js
+++ src/assets/text/inventoryText.js
@@ -1,12 +1,12 @@
 export default {
     inventoryText: {
        title: "Where is water being bottled?",
-       paragraph1: "There are many type of facilities where water is the primary part of a beverage that is bottled.  The USGS has compiled a list of facilities for several beverage types with the main focus on bottled water (Buchwald and others, 2023).",
-       paragraph2: "The bottled water industry estimates that the United States consumed 15 billion gallons (57 billion liters) of bottled water in 2020 (International Bottled Water Association). That's 45 gallons of bottled water per person.",
-       paragraph3: "Despite what we know about bottled water consumption, national datasets about how much water is withdrawn from groundwater, surface water, or springs for bottled water and other beverages are limited. Reporting is  variable, and some states do not permit public sharing of beverage-bottling data. Without these data, we cannot accurately assess bottled water influence on the water cycle locally, regionally, or nationally.",
-       paragraph4: "To help fill this gap, the USGS Water Mission Area's Bottled Water Assessment project conducted an inventory of bottling facilities across the United States (LINK TO DATA RELEASE). The inventory helps answer the following questions:",
-       bullet1: "How many beverage bottling facilities exist in the U.S., and where are they located?",
-       bullet2: "Where are these facilities sourcing their water (e.g., groundwater, spring water, public supply)?",
+       paragraph1: "There are many types of beverage-bottling facilities: breweries, wineries, soft drink manufacturing plants. What do all these facilities have in common? They all use water.",
+       paragraph2: "Bottled water alone (meaning, not part of another beverage) is an important subset of all bottling facilities. The bottled water industry estimates that the United States consumed 15 billion gallons (57 billion liters) of bottled water in 2020 (International Bottled Water Association). That's 45.2 gallons of bottled water per person, or 342 standard bottles of water.",
+       paragraph3: "Despite what we know about bottled water consumption, national datasets about how much water is withdrawn from groundwater, surface water, and springs for bottled water and other beverages are limited. Water withdrawals for bottling are not federally regulated (unlike the quality of bottled drinking water, which is regulated). Voluntary reporting is  variable, and some states do not permit public sharing of beverage-bottling data. Without these data, we cannot accurately assess bottled water influence on the water cycle locally, regionally, or nationally.",
+       paragraph4: "To help fill this gap, the USGS Water Mission Area's Bottled Water Assessment project (LINK TO DRUPAL PAGE) conducted an inventory of bottling facilities across the United States, with a main focus on bottled water (Buchwald and others, 2023). The inventory helps answer the following questions:",
+       bullet1: "How many beverage bottling facilities exist in the U.S., and where are they located? (Check out the interactive map at the top of the page ↑)",
+       bullet2: "Where are these facilities sourcing their water (for example, groundwater, spring water, or public supply)?",
        bullet3: "How much water are bottling facilities using to make their products?"
     }
 }
