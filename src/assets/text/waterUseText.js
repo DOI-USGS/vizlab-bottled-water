27cc491a391aadea9a2c11b0876d79d727a4209a
Corson-Dosch <hcorson-dosch@usgs.gov>
2023-11-08 15:15:17 -0600

    Revise water use title

--- src/assets/text/waterUseText.js
+++ src/assets/text/waterUseText.js
@@ -1,6 +1,6 @@
 export default {
     waterUseText: {
-        title: "How much water are bottling facilities using?",
+        title: "Are there data on how much water facilities use?",
         paragraph1: "Acquiring water use data can be challenging. Some states do not require reporting of some types of water use. Other states require reporting to state or local agencies but cannot share site-specific withdrawals. For bottled water and other beverage facilities, the withdrawal amounts are sometimes below reporting thresholds, or the facilities purchase water from public-supply systems, and those data are generally not available. As a result, we have limited water use data for bottling facilities."
     }
 }
